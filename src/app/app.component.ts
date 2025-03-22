f62a2685a5338eb7300290942b1d79e6d15d72e9
Shravan2506 <shravan.agarwaljhs@gmail.com>
2025-03-23 01:01:06 +0530

    [Shravan] add: adding UC-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'HelloWorld';
-  logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
-
-  openBridgeLabz() {
-    window.open('https://www.bridgelabz.com', '_blank');
-  }
+  userName: string = '';  // ✅ Initialize with an empty string
 }
