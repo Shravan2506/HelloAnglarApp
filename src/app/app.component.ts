7100675f9322c7293b16e919fbbe0228de97df0b
Shravan2506 <shravan.agarwaljhs@gmail.com>
2025-03-23 00:56:08 +0530

    [Shravan] add:adding UC-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
+
 import { Component } from '@angular/core';
- 
- @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.scss']
- })
- export class AppComponent {
-   message: string = "Hello from BridgeLabz"; 
-   title = 'HelloWorld';
-   logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'HelloWorld';
+  logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
+
+  openBridgeLabz() {
+    window.open('https://www.bridgelabz.com', '_blank');
+  }
+}
