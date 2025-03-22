55cbccf3e90e48004f27571b8ce9b70da39b8d6f
Shravan2506 <shravan.agarwaljhs@gmail.com>
2025-03-23 00:51:36 +0530

    [Shravan] add adding UC-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  message: string = "Hello from BridgeLabz"; // Data property for interpolation
-}
+ 
+ @Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
+ })
+ export class AppComponent {
+   message: string = "Hello from BridgeLabz"; 
+   title = 'HelloWorld';
+   logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';}
